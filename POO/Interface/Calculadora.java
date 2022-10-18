45db0dc01ebe2cc31f86ff2c8c2ac0b4ec476128
Diana <missernightangel@gmail.com>
2022-10-18 18:02:42 -0300

    u

---
+++ POO/Interface/Calculadora.java
@@ -0,0 +1,25 @@
+package Interface;
+
+public class Calculadora implements OperacaoMatematica {
+
+  @Override
+  public void soma(double a, double b) {
+    System.out.println("Soma: " + a + b); 
+  }
+
+  @Override
+  public void subtracao(double a, double b) {
+    System.out.println("Subtração: " + (a - b)); 
+  }
+
+  @Override
+  public void multiplicacao(double a, double b) {
+    System.out.println("Multiplicação: " + (a * b)); 
+  }
+
+  @Override
+  public void divisao(double a, double b) {
+    System.out.println("Divisão: " + (a / b)); 
+  }
+  
+}
---
+++ POO/Interface/OperacaoMatematica.java
@@ -0,0 +1,8 @@
+package Interface;
+
+interface OperacaoMatematica {
+  void soma(double a, double b);
+  void subtracao(double a, double b);
+  void multiplicacao(double a, double b);
+  void divisao(double a, double b);
+}
